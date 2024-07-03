1e44d25d1c0cac8b6f6793834895c489dbf5e208
Arsene <arskos.dev@gmail.com>
2024-07-03 16:26:25 +0000

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // Ajouter une regle pour gerer le module binaire canvas.node
+        config.module.rules.push({ test: /\.node$/, use: 'raw-loader' })
+
+        // Empecher canvas d'etre traité par Next.js dans le navigateur
+        if (!isServer) config.externals.push('canvas');
+        return config;
+
+    }
+};
 
 export default nextConfig;
+  

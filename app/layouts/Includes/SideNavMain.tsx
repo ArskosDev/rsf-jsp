0a985fbe78c065ab681886d58d68840b9f387a2c
Arsene <aappeti@gmail.com>
2024-07-08 20:54:14 +0000

    images added

--- app/layouts/Includes/SideNavMain.tsx
+++ app/layouts/Includes/SideNavMain.tsx
@@ -12,22 +12,22 @@ export default function SideNavMain() {
     return (
         <>
             <div id="SideNavMain" className={`
-                fixed z-20 bg-whitw pt-[70px] h-full lg:border-r-0 border-r-0 border-r w-[75px] overflow-auto
+                fixed z-20 bg-white pt-[70px] h-full lg:border-r-0 border-r-0 border-r w-[75px] overflow-auto
                 ${pathname === '/' ? 'lg:w-[310px]' : 'lg:w-[220px]'}
                 
                 `}>
                     <div className="lg:w-full w-[55px] mx-auto">
                         <a href="http://">
-                            <MenuItem iconString="Pour Toi"
-                            colorString={pathname == '/' ? '#F02C56' : ''}
-                            sizeString="25"/>
-                                
-                            
-                        
-                        
+                            <MenuItem iconString="Pour Toi" colorString={pathname == '/' ? '#F02C56' : ''} sizeString="25"/>        
                         </a>
+                        <a href=""><MenuItem iconString="Suivis" colorString={pathname == '/' ? '#000000' : ''} sizeString="25"/></a> 
+                        <a href=""><MenuItem iconString="Live" colorString={pathname == '/' ? '#000000' : ''} sizeString="25"/></a> 
 
                     </div>
+                    <div className="border-b lg:ml-2 mt-2"/>
+                    <h3 className="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb- px-2">Comptes Suggeres</h3>
+
+                    
 
             </div>
             

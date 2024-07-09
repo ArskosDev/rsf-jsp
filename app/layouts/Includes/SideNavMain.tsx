c6ca659ca306229f753fad439edc877b56c3e1d7
Arsene <arskos.dev@gmail.com>
2024-07-09 14:11:06 +0000

    client only component

--- app/layouts/Includes/SideNavMain.tsx
+++ app/layouts/Includes/SideNavMain.tsx
@@ -1,6 +1,7 @@
-import { Span } from "next/dist/trace";
-import { usePathname, useRouter } from "next/navigation"
+
+import { usePathname } from "next/navigation"
 import MenuItem from "./MenuItem";
+import ClientOnly from "./ClientOnly"
 
 
 export default function SideNavMain() {
@@ -21,13 +22,34 @@ export default function SideNavMain() {
                             <MenuItem iconString="Pour Toi"
                             colorString={pathname == '/' ? '#F02C56' : ''}
                             sizeString="25"/>
-                                
-                            
-                        
-                        
                         </a>
+                        <a href="http://"><MenuItem iconString="Suivis"
+                            colorString={pathname == '/' ? '#000000' : ''}
+                            sizeString="25"
+                        /></a>
+                        <a href="http://"><MenuItem iconString="Live"
+                            colorString={pathname == '/' ? '#000000' : ''}
+                            sizeString="25"
+                        /></a>
 
                     </div>
+                    
+                    <div className="border-b lg:ml-2 mt-2" />
+                    <h3 className="lg:block hidden text text-xs text-gray-600 font-se;ibold pt-4 pb-2 px-2">
+                        Comptes suggeres
+                    </h3>
+
+                    <div className="lg:hidden block pt-3"/>
+                    <ClientOnly className="cursor-pointer">
+                        <MenuItemFollow user={ { id: "1", name: "Test user", image:"https://placehold.co/50"}} />
+                    
+
+                    </ClientOnly>
+
+
+                        
+
+                    
 
             </div>
             

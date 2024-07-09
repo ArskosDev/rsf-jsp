1bf4cb281e3e23789d4b0e5c7d203e1648b90728
Arsene <arskos.dev@gmail.com>
2024-07-09 19:01:44 +0000

    post page

--- app/components/PostMain.tsx
+++ app/components/PostMain.tsx
@@ -58,6 +58,7 @@ export default function PostMain({ post}: PostMainCompType) {
                                 loop
                                 controls
                                 muted
+                                className=""
                             />
 
                         </div>

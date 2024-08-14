376ea42452ca6bcff5ee7eb3e8a729a1c33eef84
Arsene <arskos.dev@gmail.com>
2024-08-14 10:29:53 +0000

    Editer le pop-up

--- app/components/profile/EditProfileOverlay.tsx
+++ app/components/profile/EditProfileOverlay.tsx
@@ -1,6 +1,7 @@
 import { CropperDimensions, ShowErrorObject } from "@/app/types";
 import { useRouter } from "next/navigation";
 import { useState } from "react" ;
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function EditProfileOverlay(){
     const router = useRouter()
@@ -8,7 +9,7 @@ export default function EditProfileOverlay(){
     const [file, setFile] = useState <File | null>(null);
     const [cropper, setCopper] = useState <CropperDimensions | null >(null);
     const [uploadedImage, setUploadedImage] = useState<string | null >(null);
-    const [userImage, setUserImage] = useState<string | ''>('');
+    const [userImage, setUserImage] = useState<string | ''>('https://placehold.co/100');
     const [userName, setUserName] = useState<string | ''>('');
     const [userBio, setUserBio] = useState<string | ''>('');
     const [isUpdating, setIsUpdating] = useState(false);
@@ -26,7 +27,49 @@ export default function EditProfileOverlay(){
                         ${!uploadedImage ? 'h-[655px]' : 'h-[580px]'}
                         `}>
                             <div className="absolute flex items-center justify-between w-full p-5 left-0 top-0 border-b border-b-gray-300">
+                                <h1 className="text-[22px] font-medium">
+                                    Edit Profile
+                                </h1>
+                                <button
+                                    disabled={isUpdating}
+                                    className="hover:bg-gray-200 p-1 rounded-full"
+                                >
+                                    <AiOutlineClose size="25"/>
 
+                                </button>
+                            </div>
+                            <div className={`h-[calc(500px-200px)] ${!uploadedImage ? 'mt-16' : 'mt-[58px]' }`}>
+                                {!uploadedImage ? (
+                                    <div>
+                                        <div
+                                            id="ProfilePhotoSection"
+                                            className="flex flex-col border-b sm:h-[118px] h-[145px] px-1.5 py-2 w-full"
+                                        >
+                                            <h3 className="fonmt-semibold text-[15px] sm:mb-0 mb-1 text-gray-700 sm:w- [160px] sm:text-left text-center">
+                                                Profile Photo
+                                            </h3>
+
+                                            <div className="flex items-center justify-center sm:-mt-6">
+                                                <label htmlFor="image" className="relative cursor-pointer">
+                                                    <img className="rounded-full" width="95" src={userImage} />
+                                                    
+                                                    <button className="absolute bottom-0 right-0 rounded-full bg-white shadow-xl border p-1 border-gray-300 inline-block w-[32px] h-[32px]">
+
+                                                    </button>
+                                                </label>
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                ) : (
+                                    <div>
+
+                                    </div>
+
+                                )}
 
                             </div>
 

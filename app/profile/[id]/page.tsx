fff04909d7d8d59a4070397313a5f3f4434795f4
Arsene <aappeti@gmail.com>
2024-11-04 01:49:14 +0000

    corrected main layout

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -28,7 +28,7 @@ export default function Profile({ params }: ProfilePageTypes){
         <>
             
             <MainLayout>
-                <div className="pt-[90px] ml-[90px] 2xl:pl-[185px] lg:pl-[160px] lg:pr-0 w-[calc(100#-90px)] pr-3 max-w-[1800px] 2xl:mx-auto">
+                <div className="pt-[90px] ml-[90px] 2xl:pl-[185px] lg:pl-[160px] lg:pr-0 w-[calc(100%-90px)] pr-3 max-w-[1800px] 2xl:mx-auto">
                     <div className="flex w-[calc(100vw-230px)]">
                         <ClientOnly>
                             {
